4a29fd3e69918f5742c2378c345590350499bd36
Alex_Mo <doj20@mail.ru>
2023-12-07 19:56:35 +0300

    Solution with Newtonsoft

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите приложение, конвертирующее произвольный JSON в XML. Используйте JsonDocument.
+using System.Xml;
+using Newtonsoft.Json;
+
+Console.Clear();
+string targetData = FileCopyToString();
+//Console.WriteLine(targetData);
+JsonToXml(targetData);
+
+
+
+static string FileCopyToString()
+{
+    string currPath = Directory.GetCurrentDirectory();
+    string fileContent = "";
+    string[] files = Directory.GetFiles(currPath, "*.json");//try to find all .json files in the current folder
+    if (files.Length == 0)
+    {
+        Console.WriteLine("There are no any .json files in the current folder. Add at least one .json file and try again.");
+    }
+    else
+    {
+        using (StreamReader sr = new StreamReader(files[0]))
+        {
+            fileContent = sr.ReadToEnd();
+        }
+    }
+    return fileContent;
+}
+
+static void JsonToXml(string data)
+{
+    XmlDocument docXml = (XmlDocument)JsonConvert.DeserializeXmlNode(data);
+    docXml.Save(Console.Out);
+}
